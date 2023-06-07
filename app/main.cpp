631957a0abd0af2da82e7d154082c76cbd8530b6
praveen sonare <praveen@scooterson.com>
2023-06-07 17:37:25 +0800

    checking with videotestsrc

--- app/main.cpp
+++ app/main.cpp
@@ -668,8 +668,9 @@ int main(int argc, char *argv[])
 	gargv[1] = strdup("--gst-debug-level=2");
 
 	memset(pipeline_str, 0, sizeof(pipeline_str));
-	snprintf(pipeline_str, sizeof(pipeline_str), "v4l2src device=%s ! video/x-raw,width=%d,height=%d ! waylandsink", 
-		get_first_camera_device(), WINDOW_WIDTH_SIZE, WINDOW_HEIGHT_SIZE);
+	//snprintf(pipeline_str, sizeof(pipeline_str), "v4l2src device=%s ! video/x-raw,width=%d,height=%d ! waylandsink", 
+	//	get_first_camera_device(), WINDOW_WIDTH_SIZE, WINDOW_HEIGHT_SIZE);
+	snprintf(pipeline_str, sizeof(pipeline_str), "videotestsrc ! video/x-raw,width=%d,height=%d ! waylandsink", WINDOW_WIDTH_SIZE, WINDOW_HEIGHT_SIZE);
 	gst_init(&gargc, &gargv);
 
 	setbuf(stdout, NULL);
