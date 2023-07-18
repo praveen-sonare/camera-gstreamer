61ed087aaf636e5cbd3934d373045206bd4eb7aa
praveen sonare <praveen@scooterson.com>
2023-07-18 14:02:16 +0800

    http streaming

--- app/main.cpp
+++ app/main.cpp
@@ -677,19 +677,9 @@ int main(int argc, char *argv[])
 	// Below test pipeline is working with test video
 	//snprintf(pipeline_str, sizeof(pipeline_str), "videotestsrc ! video/x-raw,width=%d,height=%d ! waylandsink", WINDOW_WIDTH_SIZE, WINDOW_HEIGHT_SIZE);
 
+	// With working video
+	snprintf(pipeline_str, sizeof(pipeline_str), "souphttpsrc location=https://gstreamer.freedesktop.org/data/media/sintel_trailer-480p.mkv ! matroskademux ! theoradec ! videoconvert ! videoscale ! video/x-raw,width=%d,height=%d ! waylandsink", WINDOW_WIDTH_SIZE, WINDOW_HEIGHT_SIZE);
 
-
-	// POC area
-	// gst-launch-1.0 filesrc location=/media/UNTITLED/shrek2.mp4 ! qtdemux name=d ! queue ! v4l2h264dec ! videoconvert ! videoscale ! video/x-raw,width=640,height=720 ! waylandsink
-	// gst-launch-1.0 filesrc location=/media/UNTITLED/shrek2.mp4 ! qtdemux ! queue ! v4l2h264dec ! videoconvert ! videoscale ! waylandsink
-	//snprintf(pipeline_str, sizeof(pipeline_str), "gst-launch-1.0 v4l2src device=/dev/video0 ! videoconvert ! waylandsink");
-	//snprintf(pipeline_str, sizeof(pipeline_str), "gst-launch-1.0 playbin uri=https://gstreamer.freedesktop.org/data/media/sintel_trailer-480p.webm video-sink=\"waylandsink\"");
-	
-	//snprintf(pipeline_str, sizeof(pipeline_str), "gst-launch-1.0 filesrc location=/media/UNTITLED/shrek2.mp4 ! qtdemux ! queue ! v4l2h264dec ! videoconvert ! videoscale ! video/x-raw,width=%d,height=%d ! waylandsink",WINDOW_WIDTH_SIZE, WINDOW_HEIGHT_SIZE);
-	// audio-video pipeline (need to chosse correct audio/video decoder)
-	//snprintf(pipeline_str, sizeof(pipeline_str), "gst-launch-1.0 filesrc location=/media/UNTITLED/shrek2.mp4 ! qtdemux name=d ! queue ! avdec_h264 ! videoconvert ! videoscale ! video/x-raw,width=%d,height=%d ! autovideosink d. !  queue ! avdec_aac ! audioconvert ! audioresample ! autoaudiosink",WINDOW_WIDTH_SIZE, WINDOW_HEIGHT_SIZE);
-	//pipeline_str = "gst-launch-1.0 souphttpsrc location=https://gstreamer.freedesktop.org/data/media/sintel_trailer-480p.webm ! matroskademux name=d ! queue ! vp8dec ! videoconvert ! waylandsink d. ! queue ! vorbisdec ! audioconvert ! audioresample ! autoaudiosink";
-	//snprintf(pipeline_str, sizeof(pipeline_str), "gst-launch-1.0 souphttpsrc location=https://gstreamer.freedesktop.org/data/media/sintel_trailer-480p.webm ! matroskademux name=d ! queue ! vp8dec ! videoconvert ! waylandsink d. ! queue ! vorbisdec ! audioconvert ! audioresample ! autoaudiosink");
 	gst_init(&gargc, &gargv);
 
 	setbuf(stdout, NULL);
